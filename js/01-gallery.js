7bdd6807933234e3f8810c60041607d34ba75065
Nazariy Bakush <nazkamaz@gmail.com>
2021-12-13 21:29:11 +0200

    hw7/ex1

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryDivEl = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map((element) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${element.original}">
+    <img
+      class="gallery__image"
+      src="${element.preview}"
+      data-source="${element.original}"
+      alt="${element.description}"
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+galleryDivEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryDivEl.addEventListener("click", onClickImage);
+
+function onClickImage(event) {
+  event.preventDefault();
+
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  const largeImg = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImg}" width="800" height="600">
+`);
+  instance.show();
+
+  galleryDivEl.addEventListener("keydown", closeModal);
+
+  function closeModal(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
